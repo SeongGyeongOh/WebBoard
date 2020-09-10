d0f7ac19d0e6e7583d662d59828c1175f89c47d7
kamniang <deborahoh94@gamil.com>
2020-09-10 21:24:23 +0900

    initial commit

---
+++ saveData.php
@@ -0,0 +1,28 @@
+<?php
+    header('Content-Type:text/html; charset=utf-8');
+
+    $postTitle=$_POST['postTitle'];
+    $postName=$_POST['postName'];
+    $postDetail=$_POST['postDetail'];
+
+    // echo "$postTitle $postName $postDetail";
+
+    $conn=mysqli_connect("localhost", "kamniang", "rhdiddl12!", "kamniang");
+
+    mysqli_query($conn, 'set names utf-8');
+
+    $sql="INSERT INTO `WebBoard` (postTitle, postName, postDetail) VALUES ('$postTitle', '$postName', '$postDetail')";
+    $result=mysqli_query($conn, $sql);
+
+    if($result){
+        echo '저장됐습니다';
+    }else{
+        echo 'z...';
+    }
+
+    mysqli_close($conn);
+
+
+
+
+?>
