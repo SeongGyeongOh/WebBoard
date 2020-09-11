2ca7d4319e8fc321704812555225468d92f7fea8
kamniang <deborahoh94@gamil.com>
2020-09-11 23:36:51 +0900

    commit -succeed to change data using ajax

---
+++ loadData.php
@@ -0,0 +1,20 @@
+<?php
+    header('Content-Type:application/json; charset=utf-8');
+
+    $conn=mysqli_connect('localhost', 'kamniang', 'rhdiddl12!', 'kamniang');
+    mysqli_query($conn, "set names utf-8");
+    $sql="SELECT * FROM WebBoard";
+
+    $result=mysqli_query($conn, $sql);
+
+    $row_num=mysqli_num_rows($result);
+    for($i=0;$i<$row_num;$i++){
+        $row=mysqli_fetch_array($result, MYSQLI_ASSOC);
+        $rows[$i]=$row;
+    }
+
+    echo json_encode($rows);
+
+    mysqli_close($conn);
+
+?>
